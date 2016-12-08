5e9c6cbc1e1961e19ef8e9183d834f8d9eeaffc0
Kristin McLaughlin <mckris2@umbc.edu>
2016-12-08 15:37:28 -0500

    Update schedule_by_advisor.php

--- php/schedule_by_advisor.php
+++ php/schedule_by_advisor.php
@@ -5,6 +5,8 @@
 
 <html>
 <head><title>Advising</title>
+      <link rel='stylesheet' type='text/css' href='../html/standard.css'/>
+      <link rel='icon' type='image/png' href='../html/standard.css'/>
 <style>
 table, th, td {
 border: 1px solid black;
@@ -23,6 +25,10 @@ top:8px;
 </head>
 <body>
 
+      <div id="background">
+      <left><div id="wrapper">
+      <h1>CMNS Advising</h1>
+      
 <?php
 
 include ('mysql_connect.php');
@@ -84,5 +90,11 @@ while ($appt = mysql_fetch_array($rs))
   }
 ?>
 </table>
+            
+<h3 style='color: #FF0000;'>Copyright umbc.edu</h3>
+
+</div>
+</left>
+</div>
 </body>
 </html>
