0d83e24f45b5b29bc17bc3e3ea8e9c0fdce2f19e
Christopher Dinh <cdinh2@umbc.edu>
2016-12-12 17:47:59 -0500

    Merge branches 'ChrisUpdateBranch' and 'master' of https://github.com/RobRoseKnows/cs331-project-2 into ChrisUpdateBranch
    # Conflicts:
    #	php/view/advisor_view.php
    #	php/view/student_view.php

--- php/validate/validate_advisor_register.php
+++ php/validate/validate_advisor_register.php
@@ -29,7 +29,7 @@ if ($_POST['email'] == "")
     $error_message .= "Email field can't be blank.<br>";
 }
 
-elseif (preg_match("/^[A-Za-z0-9._+-]+@umbc\.edu$/", $_POST['email']))
+elseif (!preg_match("/^[A-Za-z0-9._+-]+@umbc\.edu$/", $_POST['email']))
 {
     $errors = True;
     $error_message .= "Not a valid UMBC Email Address<br>";
