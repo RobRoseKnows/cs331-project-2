ee45f52c38173cf923aca1304acbe78cfbfaf1d5
Kristin McLaughlin <mckris2@umbc.edu>
2016-12-19 15:03:53 -0500

    Advisors can search by date, group or advisor

--- php/processAdvisorHomepage.php
+++ php/processAdvisorHomepage.php
@@ -1,15 +1,15 @@
 <?php
+session_start();
 
 $value1 = "Schedule appointment";
 $value2 = "Print schedule";
-$value3 = "Seach appointments";
-$value4 = "Log Out";
+$value3 = "Search appointments";
 
 $value = isset($_POST['next']) ? $_POST['next'] : "";
 echo $value;
 
 if($value == $value1) {
-  header('Location: ../html/forms/add_appointment.php');
+  header('Location: view/advisor_view.php');
 } 
 
 if($value == $value2) {
@@ -17,9 +17,7 @@ if($value == $value2) {
 }
 
 if($value == $value3) {
-  header('Location: schedule_');
-}
-if($value == $value4) {
-    header('Location: ../html/forms/first_page.html');
+  $_SESSION['search'] = "";
+  header('Location: search_appts.php');
 }
 ?>
